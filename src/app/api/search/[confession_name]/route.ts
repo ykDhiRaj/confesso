ce62cd8edf56ce6cacbbd6521607518c22eee588
Dhiraj <feelsnmess@gmail.com>
2025-07-04 23:34:44 +0530

    Minor bugs

--- src/app/api/search/[confession_name]/route.ts
+++ src/app/api/search/[confession_name]/route.ts
@@ -3,16 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { confession_name: string } }
+  context: { params: { confession_name: string } }
 ) {
-  const { confession_name } = params;
-  console.log(confession_name);
+  const { confession_name } =  context.params;
 
   const { data, error } = await supabaseAdmin
     .from("confessions")
     .select("*")
     .ilike("confession_name", `%${confession_name}%`);
-    
 
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
